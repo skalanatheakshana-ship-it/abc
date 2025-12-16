24ddab606b0687cbb75f198f45792199f388aee4
skalanatheakshana-ship-it <skalanatheakshana@gmail.com>
2025-12-16 19:11:40 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 
-SESSION_ID: process.env.SESSION_ID === undefined ? 'Your SESSION ID Put' : process.env.SESSION_ID,
+SESSION_ID: process.env.𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=b8fb6349072b00ef=== undefined ? 'Your SESSION ID Put' : process.env.SESSION_ID,
 PORT: process.env.PORT === undefined ? "8000" : process.env.PORT,
 SESSION_NAME: process.env.PORT === undefined ? "asitha" : process.env.SESSION_NAME,
 };
